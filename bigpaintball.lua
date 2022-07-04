ba2ce8b036ae1356997a6ef171715a3697446f29
HydraX17 <108641316+HydraX17@users.noreply.github.com>
2022-07-04 00:02:14 -0300

    Update bigpaintball.lua

--- bigpaintball.lua
+++ bigpaintball.lua
@@ -1,4 +1,4 @@
-local library = require(game:GetService("ReplicatedStorage").Framework.Library)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("HYDRAX HUB", "DarkTheme")
 
 -- Main
