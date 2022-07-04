8016f71496475ca7ea03c49e69d7a356e8c59e99
HydraX17 <108641316+HydraX17@users.noreply.github.com>
2022-07-04 00:48:06 -0300

    Update bigpaintball.lua

--- bigpaintball.lua
+++ bigpaintball.lua
@@ -10,3 +10,17 @@ local MainSection = Main:NewSection("Main")
 MainSection:NewButton("Unlock Weapons", "Unlock all weapons", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/HydraX17/hydrax/main/paintball.lua', true))()
 end)
+
+-- Esp
+
+local Esp = Window:NewTab("Esp")
+local EspSection = Main:NewSection("Esp")
+
+
+EspSection:NewToggle("Chams ESP", "See your enemies behind walls", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
+end)
