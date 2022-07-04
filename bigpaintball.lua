aa79a1772f4efad94acb2f7edfa8b77172b46d4d
HydraX17 <108641316+HydraX17@users.noreply.github.com>
2022-07-04 00:15:04 -0300

    Update bigpaintball.lua

--- bigpaintball.lua
+++ bigpaintball.lua
@@ -3,8 +3,10 @@ local Window = Library.CreateLib("HYDRAX HUB", "DarkTheme")
 
 -- Main
 
-local Tab = Window:NewTab("Main")
+local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
+
+
 MainSection:NewButton("Unlock Weapons", "Unlock all weapons", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/HydraX17/hydrax/main/paintball.lua', true))()
 end)
