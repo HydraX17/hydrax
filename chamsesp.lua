28538556cffcf845351217b50fd278c07399abdb
HydraX17 <108641316+HydraX17@users.noreply.github.com>
2022-07-04 23:31:40 -0300

    Update chamsesp.lua

--- chamsesp.lua
+++ chamsesp.lua
@@ -1,31 +1,33 @@
-function CreateSG(name,parent,face)
-    local SurfaceGui = Instance.new("SurfaceGui",parent)
-    SurfaceGui.Parent = parent
-    SurfaceGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    SurfaceGui.Face = Enum.NormalId[face]
-	SurfaceGui.LightInfluence = 0
-	SurfaceGui.ResetOnSpawn = false
-    SurfaceGui.Name = name
-    SurfaceGui.AlwaysOnTop = true
-    local Frame = Instance.new("Frame",SurfaceGui)
-	Frame.BackgroundColor3 = Color3.fromRGB(85, 170, 255)
-	Frame.Size = UDim2.new(1,0,1,0)
-end
+local esp_settings = { ---- table for esp settings 
+    textsize = 8,
+    colour = 255,255,255
+}
 
-while wait(1) do
-for i,v in pairs (game:GetService("Players"):GetPlayers()) do
-    if v ~= game:GetService("Players").LocalPlayer and v.Character ~= nil and v.Character:FindFirstChild("Head") and v.Character.Head:FindFirstChild("cham") == nil then
-        for i,v in pairs (v.Character:GetChildren()) do
-        if v:IsA("MeshPart") or v.Name == "Head" then
-        CreateSG("cham",v,"Back")
-        CreateSG("cham",v,"Front")
-        CreateSG("cham",v,"Left")
-        CreateSG("cham",v,"Right")
-        CreateSG("cham",v,"Right")
-        CreateSG("cham",v,"Top")
-        CreateSG("cham",v,"Bottom")
-        end
-        end
+local gui = Instance.new("BillboardGui")
+local esp = Instance.new("TextLabel",gui) ---- new instances to make the billboard gui and the textlabel
+
+
+
+gui.Name = "Cracked esp"; ---- properties of the esp
+gui.ResetOnSpawn = false
+gui.AlwaysOnTop = true;
+gui.LightInfluence = 0;
+gui.Size = UDim2.new(1.75, 0, 1.75, 0);
+esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+esp.Text = ""
+esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+esp.BorderSizePixel = 4;
+esp.BorderColor3 = Color3.new(esp_settings.colour)
+esp.BorderSizePixel = 0
+esp.Font = "GothamSemibold"
+esp.TextSize = esp_settings.textsize
+esp.TextColor3 = Color3.fromRGB(esp_settings.colour) -- text colour
+
+game:GetService("RunService").RenderStepped:Connect(function() ---- loops faster than a while loop :)
+    for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+        if v ~= game:GetService("Players").LocalPlayer and v.Character.Head:FindFirstChild("Cracked esp")==nil  then -- craeting checks for team check, local player etc
+            esp.Text = "{"..v.Name.."}"
+            gui:Clone().Parent = v.Character.Head
     end
 end
-end
+end)
